69a2b6ebea92143fee6ab660c76971f6d8891e00
mosdvc <131361364+huunghia98er@users.noreply.github.com>
2024-09-03 13:13:57 +0700

    add some dto

---
+++ backend/booking-service/src/main/java/org/tour_booking/booking_service/models/request/CreateBookingRequest.java
@@ -0,0 +1,27 @@
+package org.tour_booking.booking_service.models.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * @Author: HuuNghia
+ * @LastModified: 2024/08/24
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateBookingRequest {
+    private long tourId;
+    private long customerId;
+    private LocalDate departureDate;
+    private double totalPrice;
+    private String createdBy;
+    private List<ParticipantsRequest> participants;
+}
